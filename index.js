6906bbec25545b7b3845ed1d25369e6832b98a1b
Maidi-BERRUtem <b122410365@student.utem.edu.my>
2025-03-27 14:46:27 +0800

    connected to github

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const { MongoClient } = require('mongodb');
 
+const drivers = [
+    {
+        name: "John Doe",
+        vehicleType: "Sedan",
+        isAvailable: true,
+        rating: 4.8
+    },
+    {
+        name: "Alice Smith",
+        vehicleType: "SUV",
+        isAvailable: false,
+        rating: 4.5
+    }
+];
+
+// show the data in the console
+console.log(drivers);
+
+// Show all drivers' names in the console
+drivers.forEach(driver => console.log(driver.name));
+
+// Add an additional driver
+drivers.push({
+    name: "Bob",
+    vehicleType: "Taxi",
+    isAvailable: true,
+    rating: 4.7
+});
+
 async function main() {
+
     // Replace <connection-string> with your MongoDB URI
     const uri = "mongodb://localhost:27017";
     const client = new MongoClient(uri);
